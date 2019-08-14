4cdd31211ab9cb2268470846dcf3510687f46e18
Faizal Zakaria <fai@code3.io>
2019-08-14 13:43:59 +0800

    Init commit

---
+++ lib/thermal_printer.rb
@@ -0,0 +1,6 @@
+require "thermal_printer/version"
+
+module ThermalPrinter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/thermal_printer/version.rb
@@ -0,0 +1,3 @@
+module ThermalPrinter
+  VERSION = "0.1.0"
+end
